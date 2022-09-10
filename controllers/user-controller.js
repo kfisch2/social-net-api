b7fe34357101d65654ce3b0ca1bf24ba5ed005a2
kfisch2 <kmlindsey24@gmail.com>
2022-09-09 17:13:33 -0700

    delete friend WORKS

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -112,7 +112,7 @@ const userController = {
   removeFriend({ params }, res) {
     User.findOneAndUpdate(
       { _id: params.userId },
-      { $pull: { friends: { friendId: params.friendId } } },
+      { $pull: { friends: params.friendId } },
       { new: true }
     )
       .then((dbUserData) => {
